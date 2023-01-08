2e4e9ed53399ec8266bcf565909419f56fa376ce
Wesley Francisco <wesleyhf.contato@gmail.com>
2023-01-08 01:51:50 +0100

    Scene animation

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ const app = {
     slides: [
         {
             image: null,
+            animationDuration: null,
             texts: [
                 'Você está pronto para uma aventura?',
             ],
@@ -9,6 +10,7 @@ const app = {
         },
         {
             image: 'imgs/scene-1.png',
+            animationDuration: '10s',
             texts: [
                 'A muito tempo atrás, existia um belo e próspero reino, chamado Bauru.',
                 'Onde duas almas estavam predestinadas a se encontrar.',
@@ -17,6 +19,7 @@ const app = {
         },
         {
             image: 'imgs/scene-2.png',
+            animationDuration: null,
             texts: [
                 'Um certo dia, o herói do tempo e a princesa da luz finalmente uniram suas forças.',
                 'Juntos, seus poderes cresceram e eles trouxeram luz para todo o reino.',
@@ -25,6 +28,7 @@ const app = {
         },
         {
             image: 'imgs/scene-3.png',
+            animationDuration: null,
             texts: [
                 'Anos se passaram e eles partiram em busca de aventuras, desbravando novas terras.',
                 'Mas agora eles precisam retornar, para realizar a aventura mais importante de suas vidas!',
@@ -33,6 +37,7 @@ const app = {
         },
         {
             image: null,
+            animationDuration: null,
             texts: [
                 'Mas eles precisam de heróis corajosos e destemidos para ajudar nessa jornada. Heróis como você! Você está preparado para essa aventura?',
             ],
@@ -40,7 +45,7 @@ const app = {
         },
     ],
 
-    currentIndex: 1,
+    currentIndex: 0,
     slide: null,
 
     next() {
@@ -48,6 +53,20 @@ const app = {
             this.currentIndex++;
             this.slide = this.slides[this.currentIndex];
         }
+    },
+
+    getSceneStyle(slide) {
+        const style = {};
+
+        if (slide.image) {
+            style.backgroundImage =  `url(${slide.image})`;
+        }
+
+        if (slide.animationDuration) {
+            style.animation = `${slide.animationDuration} linear forwards backgroundScrollY`;
+        }
+
+        return style;
     },
 
     init() {
